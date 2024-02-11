a5269aa5da16930b713fc979fe8090c82ff48f90
mariux02 <133933830+mariux02@users.noreply.github.com>
2024-02-11 16:39:32 -0600

    Add files via upload

---
+++ practica2_MarianaUlate/music_genre/src/main/java/music_genre/demo/MusicGenreApplication.java
@@ -0,0 +1,13 @@
+package music_genre.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicGenreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicGenreApplication.class, args);
+	}
+
+}
---
+++ practica2_MarianaUlate/music_genre/src/test/java/music_genre/demo/MusicGenreApplicationTests.java
@@ -0,0 +1,13 @@
+package music_genre.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicGenreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
